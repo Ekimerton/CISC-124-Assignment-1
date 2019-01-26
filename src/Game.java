6fb89b3b72460b1460633329112c0b803dd5a197
Ekimerton <ekim0252@gmail.com>
2019-01-25 22:03:04 -0500

    working on comments

--- src/Game.java
+++ src/Game.java
@@ -18,7 +18,8 @@ public class Game{
   int g2 = 0;
   int t1 = 0;
   int t2 = 0;
-
+  
+  //Constructor takes in two Team objects, and generates g1, g2, gets p1 and p2, uses those to generate t1 and t2.
   public Game(Team team1, Team team2){
     this.team1 = team1;
     this.team2 = team2;
@@ -30,19 +31,29 @@ public class Game{
     this.t2 = p2 + g2;
   }
 
+  /*
+  First, the t values are changed according to a randomized performance of the forwards, defenders and goalies. Then, their scores
+  (points but I didn't know hockey lol) are calculated according to the rules, using the simulateRegulation() method. Then, an if else
+  statement is entered to see if the game will go on to overtime, or if team1/team2 won. 
+  
+  If overtime is entered the getOvertimeSkill() method is run, to determine who scores the final goal. If both teams have equal overtime
+  skill, a hypothetical coin is flipped. 
+  */
   public void simulateGame(){
     t1 = + forwardsPlay() + defendersPlay() + goaliesPlay();
     t2 = + forwardsPlay() + defendersPlay() + goaliesPlay();
     int score1 = simulateRegulation(t1);
     int score2 = simulateRegulation(t2);
     if(score1 == score2){
-      if (getOvertimeSkill(team1) < getOvertimeSkill(team2)){ //Overtime when team2 wins
+      int ot1 = getOvertimeSkill(team1);
+      int ot2 = getOvertimeSkill(team2);
+      if (ot1 < ot2){ //Overtime when team2 wins
         score2++;
         team2.wins++;
         team1.overtimeLoss++;
         team2.points = team2.points + 2;
         team1.points = team1.points + 1;
-      } else if (getOvertimeSkill(team1) > getOvertimeSkill(team2)){ //Overtime when team1 wins
+      } else if (o1 > ot2){ //Overtime when team1 wins
         score1++;
         team1.wins++;
         team2.overtimeLoss++;
@@ -73,7 +84,7 @@ public class Game{
       team2.losses++;
       team1.points = team1.points + 2;
     }
-
+    //Score is added to goalsFor and goalsAgainst for scorekeeping, plus both teams games played goes up by 1.
     team1.goalsFor = team1.goalsFor + score1;
     team1.goalsAgainst = team1.goalsAgainst + score2;
     team2.goalsFor = team2.goalsFor + score2;
@@ -82,6 +93,9 @@ public class Game{
     team2.played++;
   }
 
+  /*
+  This simulates the entire season, using nested for loops, similar to a selection sort. Since selection sort compares every number to     every other number exactly once, this method uses the same methodology 
+  */
   public static void simulateSeason(){
     int count = 0;
     for(int i = 0; i < Team.teams.length; i++){
